635b65ceb13ffc3639bb4f13642a70ab22c53914
JackLovel <gogkatsu@outlook.com>
2020-04-28 07:54:08 +0800

    提交

---
+++ 作业二/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template<class T>
+void
+_print(T arg) {
+    cout << arg << " ";
+}
+
+template<class... Args>
+void
+log(Args... args) {
+    int arr[] = { (_print(args), 0)... };
+    cout << endl;
+}
+
+void
+ensure(bool condition, const string &message) {
+    if (condition) {
+        log("测试成功");
+    } else {
+        log(message);
+    }
+}
+
+// main 函数的参数是规定，复制粘贴即可
+int
+main(int argc, const char *argv[]) {
+
+    return 0;
+}
